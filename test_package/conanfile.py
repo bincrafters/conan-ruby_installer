ab80f3f0015a0a6b2bf03d1951bc6e7da8e0890c
Uilian Ries <uilianries@gmail.com>
2019-05-24 10:05:34 -0300

    Use relative path for runtime
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
 import os
-from distutils.spawn import find_executable
 from conans import ConanFile
 
 
 class RubyInstallerTestConan(ConanFile):
+    settings = "os", "arch", "build_type", "arch"
+    generators = "txt"
+
+    def  build(self):
+        pass
+
     def test(self):
-        self.output.info("which ruby: {}".format(find_executable("ruby")))
-        self.run("ruby -v")
+        self.run("ruby -v", run_environment=True)
+        self.run("ruby -e \"puts 'hello'\"", run_environment=True)
+        self.run("ruby -e \"require 'date';puts Date.today\" --disable-gems", run_environment=True)
